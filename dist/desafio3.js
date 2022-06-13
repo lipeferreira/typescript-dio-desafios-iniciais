439b4c34dd7de40b1848fed8d053914a3fffaf58
lipeferreira <felipe.ferreira1988@hotmil.com>
2022-06-13 16:02:15 -0300

    desafios dio

---
+++ dist/desafio3.js
@@ -0,0 +1,34 @@
+"use strict";
+var botaoAtualizar = document.getElementById('atualizar-saldo');
+var botaoLimpar = document.getElementById('limpar-saldo');
+var soma = document.getElementById('soma');
+var campoSaldo = document.getElementById('campo-saldo');
+var saldo = 0;
+function somarAoSaldo(soma) {
+    if (campoSaldo) {
+        saldo += soma;
+        campoSaldo.innerHTML = saldo.toString();
+        limparSoma();
+    }
+}
+function limparSoma() {
+    soma.value = '';
+}
+function limparSaldo() {
+    if (campoSaldo) {
+        saldo = 0;
+        campoSaldo.innerHTML = saldo.toString();
+    }
+}
+botaoAtualizar === null || botaoAtualizar === void 0 ? void 0 : botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
+});
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+/**
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+ */ 
---
+++ src/desafio3.ts
@@ -0,0 +1,40 @@
+let botaoAtualizar = document.getElementById('atualizar-saldo')
+let botaoLimpar = document.getElementById('limpar-saldo')!
+let soma = document.getElementById('soma')! as HTMLInputElement
+let campoSaldo = document.getElementById('campo-saldo')
+
+let saldo: number = 0
+
+function somarAoSaldo(soma: number) {
+    if(campoSaldo) {
+        saldo += soma
+        campoSaldo.innerHTML = saldo.toString()
+        limparSoma()
+    }
+}
+
+function limparSoma() {
+    soma.value = ''
+}
+
+function limparSaldo() {
+    if(campoSaldo) {
+        saldo = 0
+        campoSaldo.innerHTML = saldo.toString()
+    }
+}
+
+botaoAtualizar?.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+
+/**
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+ */
